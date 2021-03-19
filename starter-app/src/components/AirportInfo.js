67270037aac2f28d622757e3607b03b7d4c53282
Hannah <hvk5388@psu.edu>
2021-03-19 15:18:45 -0400

    beautify

--- starter-app/src/components/AirportInfo.js
+++ starter-app/src/components/AirportInfo.js
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 import './AirportInfo.css';
-// import Places from './Places';
 import Flights from './Flights';
 
 function AirportInfo() { 
+    //set the querys and responses
     const [places,setPlaces] = useState([])
     const [currencies,setCurrencies] = useState([])
-    // const [query,setQuery] = useState("")
-    // const [showPlaces,setShowPlaces] = useState(false)
     const [flights,setFlights] = useState([])
     const [query2,setQuery2] = useState("")
     const [curQuery,setCurQuery]= useState("")
@@ -16,8 +14,10 @@ function AirportInfo() {
     const [destQuery,setDestQuery] = useState("")
     const [showFlights,setShowFlights] = useState(false)
 
+    //on submit
     function handleSubmit(e){
         e.preventDefault();
+        //call api
         async function getMyAPI() {
                 const reqFlights = {
                 method:'GET',
@@ -28,6 +28,7 @@ function AirportInfo() {
                 }
             }
 
+            //get query input
             const inboundpartialdate = {
                 "inboundpartialdate": query2
             }
@@ -40,25 +41,14 @@ function AirportInfo() {
 
             const destinationplace = destQuery + '-sky'
 
-            // let response2 = await fetch('https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/'+ outboundpartialdate + '?' 
-            // + new URLSearchParams(inboundpartialdate),
-            // // + new URLSearchParams(outboundpartialdate),
-            //     reqFlights)
-
+            //create an api call link
             const link = 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/' + currency + '/en-US/' + originplace + '/' + destinationplace + '/'+ outboundpartialdate +'?' + new URLSearchParams(inboundpartialdate)
 
+            //test that the link is correct
             console.log(link)
 
-            let response2 = await fetch(link 
-            
-            // + new URLSearchParams(currencies)
-            ,
-                reqFlights)
-
-            // let response2 = await fetch('https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/' + currency + '/en-US/SFO-sky/JFK-sky/2021/03/21' 
-            // + new URLSearchParams(inboundpartialdate),
-            // // + new URLSearchParams(currency),
-            //     reqFlights)
+            //call teh api and set the response to response2
+            let response2 = await fetch(link, reqFlights)
 
             response2 = await response2.json()
             setFlights(response2.Quotes)
@@ -76,20 +66,21 @@ function AirportInfo() {
         setShowFlights(true)
     }
 
+    //returns the input form to get user querys and shows the output
     return <div className="airportinfo">
         <form onSubmit={handleSubmit}>
             <label htmlFor="inboundDate">
                 Inbound Date(yyyy-mm-dd):
             </label>
             <input id="queryInput" value={query2} onChange={(e) => setQuery2(e.target.value)} required/>
-            <label htmlFor="currency">
-                Currency:
-            </label>
-            <input id="queryInput" value={curQuery} onChange={(e) => setCurQuery(e.target.value)} required/>
             <label htmlFor="outboundDate">
                 Outbound Date(yyyy-mm-dd):
             </label>
             <input id="queryInput" value={outQuery} onChange={(e) => setOutQuery(e.target.value)} required/>
+            <label htmlFor="currency">
+                Currency:
+            </label>
+            <input id="queryInput" value={curQuery} onChange={(e) => setCurQuery(e.target.value)} required/>
             <label htmlFor="outboundCity">
                 Departure Location (Airport Code):
             </label>
@@ -100,9 +91,7 @@ function AirportInfo() {
             <input id="queryInput" value={destQuery} onChange={(e) => setDestQuery(e.target.value)} required/>
             <button className="search">Search</button>
         </form>
-        {/* {showPlaces ? <Places places={places}></Places> : <></>} */}
-        {showFlights ? <Flights flights={flights} places={places} currencies={currencies}></Flights> : <></>}
-        
+        {showFlights ? <Flights flights={flights} places={places} currencies={currencies}></Flights> : <></>}         
     </div>
 }
 
